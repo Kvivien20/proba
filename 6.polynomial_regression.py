60b7defe8142f0c163c02ab28b30a5160ceccdb2
Kriszti <herjavecz.krisztina@gmail.com>
2023-04-25 12:11:48 +0200

    main

--- 6.polynomial_regression.py
+++ 6.polynomial_regression.py
@@ -27,5 +27,7 @@ def main():
     x,y=generate_synthetic_data(x_values,coefficients)
     degrees = [1, 3, 10]
     models=[fit_polynomial_regression(x,y,degree) for degree in degrees]
-   visualize_data_and_fit(x,y,models,degrees)
+    visualize_data_and_fit(x,y,models,degrees)
 
+if __name__=="__main__":
+    main()
