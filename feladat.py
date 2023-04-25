e9e345ef2f9c8a979eae49efa4ef949cc0f5981e
Detti <bernadettkiss001@gmail.com>
2023-04-25 10:59:48 +0200

    mainelés

--- feladat.py
+++ feladat.py
@@ -14,3 +14,8 @@ def visualize_data(x,y):
     plt.title("Synthetic Data with Polynomial Relationship and Noise")
     plt.show()
 
+def main():
+    coefficients=[1,0.02,-0.002, 0.014]
+    x_values=np.linspace(-10,10,100)
+    x,y=generate_synthetic_data(x_values,coefficients)
+    visualize_data(x,y)
